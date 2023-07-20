5f9070f15720226a6686ec2f445aab45f8f8e06f
Aviion Gibbs <xanthus58@protonmail.com>
2023-07-20 23:52:17 +0930

    Squashed commit of the following:
    commit 941f3deb0c8800d29148bc9e907e555494844b1a
    Author: Aviion Gibbs <xanthus58@protonmail.com>
    Date:   Thu Jul 20 23:43:58 2023 +0930
    
        Add: Github files
    
        Added various github template files, and added an info popup alongside a back button on the login page.
    
    commit bf2d00f1f3e061b1f386216ea8aed2d7762671ec
    Author: Aviion Gibbs <xanthus58@protonmail.com>
    Date:   Thu Jul 20 22:27:32 2023 +0930
    
        Fix: Network test freezing gui
    
    commit 4705b0a95933efb4569360a6748737b53da60b43
    Author: Aviion Gibbs <xanthus58@protonmail.com>
    Date:   Thu Jul 20 21:00:00 2023 +0930
    
        Update main.rs
    
    commit ad3e6975314d807122d5c0a509e4e66ccb91cb1d
    Author: Aviion Gibbs <xanthus58@protonmail.com>
    Date:   Thu Jul 20 19:05:10 2023 +0930
    
        Update main.rs
    
    commit 935a06a226d1d8bd2b7ef1076dfb3dd6d2e523af
    Author: Aviion Gibbs <xanthus58@protonmail.com>
    Date:   Thu Jul 20 18:10:11 2023 +0930
    
        Fix!: Fix information not parsing correctly
    
        I tried to fix the information passing correctly by referring to self.tx at line 109 but now I'm receiving errors at line 102 which i have to find a way to fix too.
    
    commit f064576a9cf4651c6cedc0964eccc799dc1860cd
    Author: Aviion Gibbs <xanthus58@protonmail.com>
    Date:   Thu Jul 20 17:58:23 2023 +0930
    
        Fix!: Freezing application
    
        Trying to fix the app from freezing when probing an ip for a valid connection

--- src/main.rs
+++ src/main.rs
@@ -25,23 +25,27 @@ struct MyApp {
     closable: bool,
     duration: f32,
 
-    server_bool: bool,
+    server_valid: bool,
     ip: String,
     port: i32,
 
     login_bool: bool,
     username: String,
     password: String,
+
+    tx: mpsc::Sender<bool>,
+    rx: mpsc::Receiver<bool>,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
+        let (tx, rx) = mpsc::channel();
         Self {
             toasts: Toasts::default().with_anchor(Anchor::BottomRight),
             closable: true,
             duration: 3.5,
 
-            server_bool: false,
+            server_valid: false,
             login_bool: false,
 
             ip: "127.0.0.1".to_owned(),
@@ -49,6 +53,9 @@ impl Default for MyApp {
 
             username: "".to_owned(),
             password: "".to_owned(),
+
+            tx,
+            rx,
         }
     }
 }
@@ -64,22 +71,24 @@ impl eframe::App for MyApp {
                     .set_duration(Some(Duration::from_millis((1000. * self.duration) as u64)));
             };
 
-            if self.server_bool == false {
+            if !self.server_valid {
                 ui.heading("Vault GUI");
                 ui.label("Please enter the ip and port of the sql server below");
                 ui.horizontal(|ui| {
-                    ui.label("IP: ");
+                    ui.label("IP:");
                     ui.text_edit_singleline(&mut self.ip);
                 });
 
                 ui.horizontal(|ui| {
-                    ui.label("Port: ");
+                    ui.label("Port:");
                     ui.add(egui::DragValue::new(&mut self.port).speed(1.0).clamp_range(0..=65535));
                 });
 
                 if ui.button("Connect").clicked() {
+                    cb(self.toasts.info("Testing connection..."));
+
                     let ip_verified = validate_ip_address(&self.ip);
-                    if ip_verified == false {
+                    if !ip_verified {
                         cb(self.toasts.error("Invalid IP Address!"));
                         println!("Invalid IP Address!");
                         return
@@ -93,45 +102,51 @@ impl eframe::App for MyApp {
                     let port = self.port;
     
                     // Create a channel to communicate the result of the ping test
-                    let (tx, rx) = mpsc::channel();
+                    let tx = self.tx.clone();
                     
                     //TODO: Fix this async code and try to make it not freeze the gui
                     thread::spawn(move || {
                         let result = tokio::runtime::Runtime::new()
                             .unwrap()
                             .block_on(is_server_alive(ip, port as u16, 5));
-                        tx.send(result).unwrap();
+                        tx.send(dbg!(result)).expect("Failed to send result");
                     });
-                    
-                    if let Ok(result) = rx.recv_timeout(Duration::from_secs(30)) {
-                        if result {
-                            cb(self.toasts.success("Connection Successful!"));
-                            self.server_bool = true;
-                            println!("Connection Successful!")
-                        } else {
-                            cb(self.toasts.error("Connection Failed!"));
-                            println!("Connection Failed!")
-                        }
-                    } else {
-                        cb(self.toasts.error("Connection Timed Out!"));
-                        println!("Connection Timed Out!")
-                    }
                 }
-            } 
+            }
 
-            if self.login_bool == false && self.server_bool == true {
+            if !self.login_bool && self.server_valid {
                 ui.label("Please enter your username and password below");
+
                 ui.horizontal(|ui| {
-                    ui.label("Username: ");
+                    ui.label("Username:");
                     ui.text_edit_singleline(&mut self.username);
                 });
+
                 ui.horizontal(|ui| {
-                    ui.label("Password: ");
+                    ui.label("Password:");
                     ui.add(egui::TextEdit::singleline(&mut self.password).password(true));
                 });
-                if ui.button("Login").clicked() {
-                    println!("Username: {}", self.username);
-                    println!("Password: {}", self.password);
+
+                ui.horizontal(|ui| {
+                    if ui.button("Login").clicked() {
+                        println!("Username: {}", self.username);
+                        println!("Password: {}", self.password);
+                } 
+
+                    if ui.button("Return").clicked() {
+                        self.server_valid = false;
+                }
+                });
+            }
+
+            if let Ok(result) = self.rx.try_recv() {
+                if result {
+                    cb(self.toasts.success("Connection Successful!"));
+                    self.server_valid = true;
+                    println!("Connection Successful!")
+                } else {
+                    cb(self.toasts.error("Connection Failed!"));
+                    println!("Connection Failed!")
                 }
             }
             
@@ -139,4 +154,3 @@ impl eframe::App for MyApp {
         self.toasts.show(ctx); // Requests to render toasts
     }
 }
-
